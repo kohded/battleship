dd6ffade789886b6ce9a721215b60ed36f11155e
kohded <arnold@kohded.com>
2015-11-14 17:38:55 -0800

    Created player and ships class

---
+++ src/Player.java
@@ -0,0 +1,6 @@
+public abstract class Player implements BattleshipMain {
+
+    public void placeShip(BattleshipMain.ships ship, playerInterface.Direction d, int startX, int startY) {
+
+    }
+}
---
+++ src/Ships.java
@@ -0,0 +1,3 @@
+public abstract class Ships implements BattleshipMain {
+    
+}
